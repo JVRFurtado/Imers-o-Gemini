95f40d2dc9b773cdfc17fbeb78a2131b5f941569
JVRFurtado <joao.furtado546@gmail.com>
2024-09-06 23:20:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 function pesquisar() {
     // Obtém a seção HTML onde os resultados da pesquisa serão exibidos.
     let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se o campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar um nome de um atleta ou eporte.</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
    
     // Inicializa uma string vazia para armazenar os resultados formatados em HTML.
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre cada elemento (dado) dentro da lista de dados.
     for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+    if (dado.titulo.includes(campoPesquisa) || dado.descricao.includes(campoPesquisa) || dado.tags.includes(campoPesquisa)) {
         // Concatena o HTML para cada resultado, formatando os dados do objeto 'dado'.
         resultados += `
             <div class="item-resultado">
@@ -17,8 +35,13 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
         `;
+        }
+        
     }
 
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
     // Atribui o HTML gerado para o conteúdo interno da seção de resultados.
     section.innerHTML = resultados;
 }
--- dados.js
+++ dados.js
@@ -1,17 +1,20 @@
 let dados = [
     {
-    titulo : "Gabriel Geraldo dos Santos Araújo",
-    descricao : "Gabriel Geraldo dos Santos Araújo, mais conhecido como Gabrielzinho, é um dos maiores nomes da natação paralímpica brasileira. Nascido em Santa Luzia, Minas Gerais, o atleta conquistou o país com suas performances excepcionais nas piscinas, coletando diversas medalhas em competições nacionais e internacionais. Sua dedicação e talento o tornaram um verdadeiro ídolo, inspirando milhares de pessoas com suas conquistas. Com um currículo repleto de ouros e pratas, Gabrielzinho é um exemplo de superação e determinação, provando que com força de vontade, qualquer objetivo pode ser alcançado.",
-    link : "https://pt.wikipedia.org/wiki/Gabriel_Ara%C3%BAjo"
+    titulo: "Gabriel Geraldo dos Santos Araújo",
+    descricao: "Gabriel Geraldo dos Santos Araújo, mais conhecido como Gabrielzinho, é um dos maiores nomes da natação paralímpica brasileira. Nascido em Santa Luzia, Minas Gerais, o atleta conquistou o país com suas performances excepcionais nas piscinas, coletando diversas medalhas em competições nacionais e internacionais. Sua dedicação e talento o tornaram um verdadeiro ídolo, inspirando milhares de pessoas com suas conquistas. Com um currículo repleto de ouros e pratas, Gabrielzinho é um exemplo de superação e determinação, provando que com força de vontade, qualquer objetivo pode ser alcançado.",
+    link: "https://pt.wikipedia.org/wiki/Gabriel_Ara%C3%BAjo",
+    tags: "ouro nadador recordista mundial"
 },
     {
-    titulo : "Maria Carolina Gomes Santiago",
-    descricao : "Maria Carolina Gomes Santiago é uma das maiores atletas paralímpicas brasileiras de todos os tempos. Com um talento inegável e uma determinação ímpar, a nadadora pernambucana conquistou o mundo com suas performances excepcionais nas piscinas. Campeã em diversas provas e detentora de recordes mundiais, Carol Santiago se tornou um símbolo de superação e inspiração para milhões de pessoas. Sua trajetória nas Paralimpíadas é marcada por conquistas históricas, consolidando seu lugar como uma das maiores representantes do esporte paralímpico brasileiro.",
-    link : "https://pt.wikipedia.org/wiki/Carol_Santiago"
+    titulo: "Maria Carolina Gomes Santiago",
+    descricao: "Maria Carolina Gomes Santiago é uma das maiores atletas paralímpicas brasileiras de todos os tempos. Com um talento inegável e uma determinação ímpar, a nadadora pernambucana conquistou o mundo com suas performances excepcionais nas piscinas. Campeã em diversas provas e detentora de recordes mundiais, Carol Santiago se tornou um símbolo de superação e inspiração para milhões de pessoas. Sua trajetória nas Paralimpíadas é marcada por conquistas históricas, consolidando seu lugar como uma das maiores representantes do esporte paralímpico brasileiro.",
+    link: "https://pt.wikipedia.org/wiki/Carol_Santiago",
+    tags: "ouro natação recordista mundial"
 },
     {
-    titulo : "Elizabeth Rodrigues Gomes",
-descricao : "Elizabeth Rodrigues Gomes, conhecida carinhosamente como Beth Gomes, é uma das maiores referências do atletismo paralímpico brasileiro. Com uma trajetória inspiradora, a atleta paulista conquistou o mundo com suas performances excepcionais no arremesso de peso e lançamento de disco. Aos 56 anos, Beth se tornou um símbolo de superação e determinação, provando que a idade não é um obstáculo para alcançar grandes feitos. Medalhista paralímpica e recordista mundial, ela é um exemplo de que o esporte paralímpico é capaz de transformar vidas e inspirar milhões de pessoas. Sua participação nos Jogos Paralímpicos de Tóquio 2020, onde conquistou o ouro no lançamento de disco, marcou a história do esporte brasileiro.",
-link : "https://pt.wikipedia.org/wiki/Elizabeth_Gomes"
+    titulo: "Elizabeth Rodrigues Gomes",
+    descricao: "Elizabeth Rodrigues Gomes, conhecida carinhosamente como Beth Gomes, é uma das maiores referências do atletismo paralímpico brasileiro. Com uma trajetória inspiradora, a atleta paulista conquistou o mundo com suas performances excepcionais no arremesso de peso e lançamento de disco. Aos 56 anos, Beth se tornou um símbolo de superação e determinação, provando que a idade não é um obstáculo para alcançar grandes feitos. Medalhista paralímpica e recordista mundial, ela é um exemplo de que o esporte paralímpico é capaz de transformar vidas e inspirar milhões de pessoas. Sua participação nos Jogos Paralímpicos de Tóquio 2020, onde conquistou o ouro no lançamento de disco, marcou a história do esporte brasileiro.",
+    link: "https://pt.wikipedia.org/wiki/Elizabeth_Gomes",
+    tags: "ouro arremesso de dardos arremesso de peso"
 }
 ];
